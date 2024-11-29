8bd010c7fca358ba64c38c4458398e2b08e61239
KenasGaleno <150374973+KenasGaleno@users.noreply.github.com>
2024-11-29 21:55:27 +0700

    Update AddBookView.vue

--- src/views/AddBookView.vue
+++ src/views/AddBookView.vue
@@ -81,65 +81,75 @@ export default defineComponent({
 
 <template>
     <main class="mt-12 mx-8 pb-20">
-        <h1 class="text-4xl font-extrabold mb-8 text-center text-blue-1000">Add a New Book</h1>
-        <form @submit.prevent="addBook" class="max-w-xl mx-auto bg-white p-6 rounded-lg shadow-lg space-y-4">
+        <h1 class="text-4xl font-extrabold mb-8 text-center text-white">Add a New Book</h1>
+        <form @submit.prevent="addBook" class="max-w-xl mx-auto bg-gray-900 p-6 rounded-lg shadow-lg space-y-4">
             <div class="flex flex-col">
-                <label for="title" class="text-lg font-bold text-gray-700">Title</label>
+                <label for="title" class="text-lg font-bold text-white">Title</label>
                 <input v-model="title" type="text" id="title" 
-    class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-    required />
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="author" class="text-lg font-bold text-gray-700">Author</label>
-                <input v-model="author" type="text" id="author" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="author" class="text-lg font-bold text-white">Author</label>
+                <input v-model="author" type="text" id="author" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="publishedDate" class="text-lg font-semibold text-gray-700">Published Date</label>
-                <input v-model="publishedDate" type="date" id="publishedDate" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="publishedDate" class="text-lg font-bold text-white">Published Date</label>
+                <input v-model="publishedDate" type="date" id="publishedDate" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="publisher" class="text-lg font-semibold text-gray-700">Publisher</label>
-                <input v-model="publisher" type="text" id="publisher" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="publisher" class="text-lg font-bold text-white">Publisher</label>
+                <input v-model="publisher" type="text" id="publisher" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="description" class="text-lg font-semibold text-gray-700">Description</label>
-                <textarea v-model="description" id="description" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="description" class="text-lg font-bold text-white">Description</label>
+                <textarea v-model="description" id="description" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required></textarea>
             </div>
             <div class="flex flex-col">
-                <label for="coverImage" class="text-lg font-semibold text-gray-700">Cover Image</label>
-                <input v-model="coverImage" type="text" id="coverImage" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="coverImage" class="text-lg font-bold text-white">Cover Image</label>
+                <input v-model="coverImage" type="text" id="coverImage" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="ratingAverage" class="text-lg font-semibold text-gray-700">Rating Average</label>
-                <input v-model="ratingAverage" type="number" step="0.1" id="ratingAverage" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="ratingAverage" class="text-lg font-bold text-white">Rating Average</label>
+                <input v-model="ratingAverage" type="number" step="0.1" id="ratingAverage" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="ratingCount" class="text-lg font-semibold text-gray-700">Rating Count</label>
-                <input v-model="ratingCount" type="number" id="ratingCount" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="ratingCount" class="text-lg font-bold text-white">Rating Count</label>
+                <input v-model="ratingCount" type="number" id="ratingCount" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="tags" class="text-lg font-semibold text-gray-700">Tags (comma separated)</label>
-                <input v-model="tags" type="text" id="tags" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="tags" class="text-lg font-bold text-white">Tags (comma separated)</label>
+                <input v-model="tags" type="text" id="tags" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="initialQty" class="text-lg font-semibold text-gray-700">Initial Quantity</label>
-                <input v-model="initialQty" type="number" id="initialQty" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="initialQty" class="text-lg font-bold text-white">Initial Quantity</label>
+                <input v-model="initialQty" type="number" id="initialQty" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
             <div class="flex flex-col">
-                <label for="qty" class="text-lg font-semibold text-gray-700">Quantity</label>
-                <input v-model="qty" type="number" id="qty" class="mt-2 p-4 border-2 border-gray-500 rounded-lg text-gray-700 focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
-                required />
+                <label for="qty" class="text-lg font-bold text-white">Quantity</label>
+                <input v-model="qty" type="number" id="qty" 
+                    class="mt-2 p-4 border-2 border-gray-700 rounded-lg bg-gray-800 text-white focus:ring-2 focus:ring-blue-500 shadow-md hover:border-blue-500 transition" 
+                    required />
             </div>
-            <div v-if="error" class="mt-4 bg-red-100 text-red-500 p-3 rounded-lg">
+            <div v-if="error" class="mt-4 bg-red-500 text-white p-3 rounded-lg">
                 {{ errorMsg }}
             </div>
             <button type="submit" class="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition">Add Book</button>
